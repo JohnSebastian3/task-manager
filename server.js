a2c32e5c25bf30a254505d8ba3f32d099dff3bd7
Sebastian <sebastianbiochem@gmail.com>
2022-06-15 18:31:15 -0400

    Fix css link

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const express = require("express");
+const express = require('express');
 const { request } = require("http");
 const app = express();
 const MongoClient = require('mongodb').MongoClient;
